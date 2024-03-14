ded4420d9bf7ad9940947e0440a05ffb9604ec19
harshinie <harshinie385@gmail.com>
2024-03-14 22:09:07 +0530

    Concept

--- app.js
+++ app.js
@@ -4,9 +4,22 @@ const myName = "Harshinie"
 
 let num = 5
 
-console.log(myName)
-console.log(num)
+// console.log(myName)
+// console.log(num)
+
+
 
 num = 10
 
-console.log(num)
+// console.log(num)
+
+// Data types
+
+let myNumber;
+console.log(myNumber) // undefined
+
+let boolValue = true
+let nullValue = null
+
+console.log(boolValue)
+console.log(nullValue)
