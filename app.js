af55ffd30e7b98471131ecd1a7cd16a6d41fd6e7
harshinie <harshinie385@gmail.com>
2024-03-16 21:21:29 +0530

    Array

--- app.js
+++ app.js
@@ -16,10 +16,22 @@ num = 10
 // Data types
 
 let myNumber;
-console.log(myNumber) // undefined
+//console.log(myNumber) // undefined
 
 let boolValue = true
 let nullValue = null
 
-console.log(boolValue)
-console.log(nullValue)
+
+//console.log(boolValue)
+//console.log(nullValue)
+// Array
+
+let arr = [1,20,"John","blue"]
+//console.log(arr)
+
+let integerArray = [2, 5, 7, 9, 11]
+
+console.log(integerArray.length)
+console.log(integerArray[0])
+console.log(integerArray[1])
+console.log(integerArray[integerArray.length-1])
